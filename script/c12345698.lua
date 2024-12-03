3adbe0c55521fb3d2213e6207ebe2e35543cfa34
shemladavid <156425811+shemladavid@users.noreply.github.com>
2024-12-03 17:13:04 +0200

    דגכע

--- script/c12345698.lua
+++ script/c12345698.lua
@@ -1,8 +1,8 @@
 -- Gate of the Celestial Summoner
-local s,id=GetID()
+local s, id = GetID()
 function s.initial_effect(c)
     -- Activate effect
-    local e1=Effect.CreateEffect(c)
+    local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
@@ -11,26 +11,35 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
-function s.filter(c,e,tp)
-    return c:IsLocation(LOCATION_EXTRA) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,true,false)
+function s.filter(c, e, tp)
+    return c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_SPECIAL, tp, true, false)
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-            and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_EXTRA, 0, 1, nil, e, tp)
     end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_EXTRA)
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-    local tc=g:GetFirst()
+function s.activate(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_EXTRA, 0, 1, 1, nil, e, tp)
+    local tc = g:GetFirst()
     if tc then
-        Duel.SpecialSummon(tc,SUMMON_TYPE_SPECIAL,tp,tp,true,false,POS_FACEUP)
-        -- Ignore summon condition
+        local summon_type = 0
+        if tc:IsType(TYPE_FUSION) then
+            summon_type = SUMMON_TYPE_FUSION
+        elseif tc:IsType(TYPE_SYNCHRO) then
+            summon_type = SUMMON_TYPE_SYNCHRO
+        elseif tc:IsType(TYPE_XYZ) then
+            summon_type = SUMMON_TYPE_XYZ
+        elseif tc:IsType(TYPE_LINK) then
+            summon_type = SUMMON_TYPE_LINK
+        end
+        Duel.SpecialSummon(tc, summon_type, tp, tp, true, false, POS_FACEUP)
         tc:CompleteProcedure()
     end
 end
