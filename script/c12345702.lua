54b1e0c99b2383a3c25ded28e2924734726c1828
shemladavid <156425811+shemladavid@users.noreply.github.com>
2024-12-31 12:38:22 +0200

    wet

--- script/c12345702.lua
+++ script/c12345702.lua
@@ -18,12 +18,21 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,1)
 	e2:SetCondition(s.trsumcon)
-	e2:SetValue(1)
+	e2:SetTarget(s.sumlimit)
 	c:RegisterEffect(e2)
+	--Track Extra Deck Summons
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
 	--Banish opponent's card face-down
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
@@ -78,6 +87,18 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 			local sc=sg:Select(tp,1,1,nil):GetFirst()
 			Duel.Summon(tp,sc,true,nil) 
 		end
+	end
+end
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA) and Duel.GetFlagEffect(sump,id)>=1
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsSummonLocation(LOCATION_EXTRA) then
+			Duel.RegisterFlagEffect(tc:GetSummonPlayer(),id,RESET_PHASE+PHASE_END,0,1)
+		end
+		tc=eg:GetNext()
 	end
 end
 function s.banishcon(e,tp,eg,ep,ev,re,r,rp)
