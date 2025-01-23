279af1f7acd8de700021f0a3678420e9c44d813d
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-01-24 01:44:28 +0200

    h

--- script/c12345644.lua
+++ script/c12345644.lua
@@ -15,6 +15,16 @@ function s.initial_effect(c)
 	e2:SetCondition(s.rcon)
 	e2:SetOperation(s.repop)
 	c:RegisterEffect(e2)
+
+    -- Attach top card of opponent's deck as material
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_CHAIN_SOLVING)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCondition(s.matcon)
+    e3:SetOperation(s.matop)
+    c:RegisterEffect(e3)
+
 end
 
 function s.rcon(e,tp,eg,ep,ev,re,r,rp)
@@ -24,4 +34,20 @@ end
 
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Recover(tp,500,REASON_EFFECT)
+end
+
+function s.matcon(e,tp,eg,ep,ev,re,r,rp)
+    local rc=re:GetHandler()
+    return rc:IsControler(tp) and rc:IsType(TYPE_XYZ) and re:IsActivated()
+end
+
+function s.matop(e,tp,eg,ep,ev,re,r,rp)
+    local rc=re:GetHandler()
+    if rc:IsRelateToEffect(re) then
+        local g=Duel.GetDecktopGroup(1-tp,1)
+        if #g>0 then
+            Duel.DisableShuffleCheck()
+            Duel.Overlay(rc,g)
+        end
+    end
 end
