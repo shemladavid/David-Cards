3f7470311e8c08d9a425ee1fccce8c070ff004f6
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-01-24 02:00:06 +0200

    hh

--- script/c12345644.lua
+++ script/c12345644.lua
@@ -7,47 +7,40 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
 
-    -- Optional detach replacement effect
+    -- Attach cards from opponent's deck when materials are detached
     local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_OVERLAY_REMOVE_REPLACE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.rcon)
-	e2:SetOperation(s.repop)
-	c:RegisterEffect(e2)
-
-    -- Attach top card of opponent's deck as material
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e3:SetCode(EVENT_CHAIN_SOLVING)
-    e3:SetRange(LOCATION_SZONE)
-    e3:SetCondition(s.matcon)
-    e3:SetOperation(s.matop)
-    c:RegisterEffect(e3)
-
-end
-
-function s.rcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(id+ep)==0
-		and (r&REASON_COST)~=0 and re:IsActivated() and re:IsActiveType(TYPE_XYZ)
-end
-
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Recover(tp,500,REASON_EFFECT)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_OVERLAY_REMOVE)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCondition(s.matcon)
+    e2:SetTarget(s.mattg)
+    e2:SetOperation(s.matop)
+    c:RegisterEffect(e2)
 end
 
 function s.matcon(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    return rc:IsControler(tp) and rc:IsType(TYPE_XYZ) and re:IsActivated()
+    local rc=eg:GetFirst()
+    return rc:IsControler(tp) and rc:IsType(TYPE_XYZ) and eg:IsContains(rc)
+end
+
+function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_ATTACH,nil,1,1-tp,LOCATION_DECK)
 end
 
 function s.matop(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
+    local rc=eg:GetFirst()
     if rc:IsRelateToEffect(re) then
         local g=Duel.GetDecktopGroup(1-tp,1)
-        if #g>0 then
+        local ct=0
+        while #g>0 do
             Duel.DisableShuffleCheck()
             Duel.Overlay(rc,g)
+            ct=ct+1
+            g=Duel.GetDecktopGroup(1-tp,1)
+        end
+        if ct>0 then
+            Duel.Recover(tp,1000*ct,REASON_EFFECT)
         end
     end
 end
