b3596cc3bebb6fdf4342ff1c744b4c938afe0b3d
shemladavid <156425811+shemladavid@users.noreply.github.com>
2025-01-26 14:52:29 +0200

    mn,v

--- script/c12345644.lua
+++ script/c12345644.lua
@@ -18,27 +18,65 @@ function s.initial_effect(c)
     e2:SetTarget(s.target)
     e2:SetOperation(s.operation)
     c:RegisterEffect(e2)
+
+    -- Destroy opponent's card
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCountLimit(1)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetTarget(s.destg)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
 end
 
 function s.condition(e, tp, eg, ep, ev, re, r, rp)
-    local rc = eg:GetFirst()
-    return rc:IsControler(tp) and rc:IsType(TYPE_XYZ) and eg:IsContains(rc)
+    return eg:IsExists(Card.IsType, 1, nil, TYPE_XYZ) and eg:IsExists(Card.IsControler, 1, nil, tp)
 end
 
 function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.GetFieldGroupCount(tp, 0, LOCATION_DECK) > 0
+        return Duel.GetFieldGroupCount(tp, 0, LOCATION_DECK) >= 2
     end
 end
 
 function s.operation(e, tp, eg, ep, ev, re, r, rp)
-    local tc = eg:GetFirst()
-    if Duel.GetFieldGroupCount(tp, 0, LOCATION_DECK) == 0 then
-        return
+    local g = Duel.GetDecktopGroup(1 - tp, 2) -- Get the top 2 cards of opponent's deck
+    if #g == 0 then return end
+
+    -- Attach 2 new cards for each Xyz monster involved
+    local processed = Group.CreateGroup() -- Track monsters that have already received cards
+    for tc in aux.Next(eg) do
+        if tc:IsControler(tp) and tc:IsType(TYPE_XYZ) and not processed:IsContains(tc) then
+            Duel.Overlay(tc, g) -- Attach the cards to the current Xyz monster
+            Duel.Recover(tp, #g * 1000, REASON_EFFECT) -- Recover life points
+            processed:AddCard(tc) -- Mark this monster as processed
+
+            -- Refresh the top 2 cards for the next monster
+            g = Duel.GetDecktopGroup(1 - tp, 2)
+            if #g == 0 then break end
+        end
     end
-    local g = Duel.GetDecktopGroup(1 - tp, 2)
-    if #g > 0 then
-        Duel.Overlay(tc, g)
-        Duel.Recover(tp, #g * 1000, REASON_EFFECT)
+end
+
+function s.destg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chkc then return chkc:IsOnField() and chkc:IsControler(1 - tp) end
+    if chk == 0 then return Duel.IsExistingTarget(nil, tp, 0, LOCATION_ONFIELD, 1, nil) end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g = Duel.SelectTarget(tp, nil, tp, 0, LOCATION_ONFIELD, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
+end
+
+function s.desop(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        local xyz = Duel.SelectMatchingCard(tp, aux.FaceupFilter(Card.IsType, TYPE_XYZ), tp, LOCATION_MZONE, 0, 1, 1, nil):GetFirst()
+        if xyz and xyz:CheckRemoveOverlayCard(tp, 1, REASON_EFFECT) then
+            xyz:RemoveOverlayCard(tp, 1, 1, REASON_EFFECT)
+            Duel.Destroy(tc, REASON_EFFECT)
+        end
     end
 end
